4451e27bec9a3352f7176160e6757b757c9a873e
Parag Kulkarni <Parag.Kulkarni@opusconsulting.com>
2018-09-19 23:41:52 +0530

    updated server with http or https options

--- hopeandcure_server/hms_server.js
+++ hopeandcure_server/hms_server.js
@@ -1,28 +1,39 @@
 #!/usr/bin/env node
 
 var https = require('https');
+var http = require('http');
 var fs = require('fs');
 var express = require('express');
 
-// Instantiate our server
-//var server = restify.createServer();
-
-var options = {
-  key: fs.readFileSync( '/etc/certs/localhost.key' ),
-  cert: fs.readFileSync( '/etc/certs/localhost.cert' ),
-  requestCert: false,
-  rejectUnauthorized: false
-};
-
-
+// define constants
+const tls_or_http = 'https'; // change to https when necessary
+const http_port = 8082;
+const https_port = 7443;
 var app = express();
-var port = process.env.PORT || 7443;
-var server = https.createServer( options, app );
-var router = require('./router.js')(app);
+
+if (tls_or_http === 'https') {
+
+  var options = {
+    //key: fs.readFileSync('/etc/certs/localhost.key'),
+    //cert: fs.readFileSync('/etc/certs/localhost.cert'),
+    key: fs.readFileSync('D:\\Parag\\Personal\\hmsapp\\localhost.key'),
+    cert: fs.readFileSync('D:\\Parag\\Personal\\hmsapp\\localhost.cert'),
+    requestCert: false,
+    rejectUnauthorized: false
+  };
+
+  var port = process.env.PORT || https_port;
+  var server = https.createServer(options, app);
+  var router = require('./router.js')(app);
+
+} else if (tls_or_http === 'http') {
+  var port = process.env.PORT || http_port;
+  var server = http.createServer(app);
+  var router = require('./router.js')(app);
+
+}
 
 // Now, setup server
-//setup_server(server);
-
-server.listen( port, function (app) {
-  console.log( 'Express server listening on port ' + server.address().port );
-} );
+server.listen(port, function (app) {
+  console.log('Express server listening on port ' + server.address().port);
+});
